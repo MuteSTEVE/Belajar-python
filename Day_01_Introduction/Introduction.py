00640be4c7c273a76a21a906673643384d1a67a5
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-11-23 20:03:27 +0700

    Day 1 - Introduction

---
+++ Day_01_Introduction/Introduction.py
@@ -0,0 +1,52 @@
+a = 3
+b = 4
+
+add = a + b
+sub = a - b
+multi = a * b
+mod  = a % b
+div = a / b
+exp = a ** b
+floor  = a // b
+
+print(add)
+print(sub)
+print(multi)
+print(mod)
+print(div)
+print(exp)
+print(floor)
+
+name = "Satrio"
+family = "Hendarto"
+country = "Indonesia"
+lang = "Python"
+text = "I am enjoying 30 days of python"
+
+print(name)
+print(family)
+print(country)
+print(lang)
+print(text)
+
+val_1 = 10
+val_2 = 9.8
+val_3 = 3.14
+val_4 = 4 - 4j
+val_5 = [name, lang, country ]
+
+print(val_1)
+print(val_2)
+print(val_3)
+print(val_4)
+print(val_5)
+print(name)
+print(family)
+print(country)
+
+# Finding euclidean distance
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+d = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
+print(d)
