6be95052f0b2eb39dd608add0c24df4e5f37c567
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-11-23 11:07:06 +0700

    01_Day_Introduction

---
+++ input.py
@@ -0,0 +1,12 @@
+# data = input("What's your name: ")
+
+# print(f"Your name is: {data}, {type(data)}")
+
+# boolean = bool(int(input("1 or 0: ")))
+
+# print(f"data = {boolean}, type = {type(boolean)}")
+
+angka = float(input("Masukan angka: "))
+angka = int(input("Masukan angka: "))
+
+print(f"data = {angka}, type = {type(angka)}")
